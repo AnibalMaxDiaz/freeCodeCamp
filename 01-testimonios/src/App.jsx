4b05fc3d5de6c84ea98e5c08397f8c46b4849221
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2026-04-22 19:54:34 -0500

    all css and js files in the project finished

--- 01-testimonios/src/App.jsx
+++ 01-testimonios/src/App.jsx
@@ -3,35 +3,34 @@ import Testimony from "./components/Testimony.jsx";
 function App() {
     return (
     <>
-      <div className="main-container" >
-        <h1>Here's what our students are saying about freeCodeCamp:</h1>
-        <Testimony           
-          name="Shawn"
-          country="Singapore"
-          // image="Shawn"
-          position="Software Engineer"
-          enterprice="Amazon"
-          text="It's scary to change careers. I only gained the confidence that I could program by working through the hundreds of hours of free lessons in freeCodeCamp. Within a year I had a six-figure job as a software engineer. freeCodeCamp changed my life."
-        />
+      <div className="App">
+        <div className="main-container" >
+          <h1>Here's what our students are saying about freeCodeCamp:</h1>
+          <Testimony           
+            name="Shawn"
+            country="Singapore"
+            position="Software Engineer"
+            enterprice="Amazon"
+            text="It's scary to change careers. I only gained the confidence that I could program by working through the hundreds of hours of free lessons in freeCodeCamp. Within a year I had a six-figure job as a software engineer. freeCodeCamp changed my life."
+          />
 
-        <Testimony
-          name="Sarah"
-          country="United States"
-          // image="Sarah"
-          position="Web Developer"
-          enterprice="Google"
-          text="freeCodeCamp provided me with the skills and confidence I needed to transition into web development. The community support and hands-on projects were invaluable."
-        />
+          <Testimony
+            name="Sarah"
+            country="United States"
+            position="Web Developer"
+            enterprice="Google"
+            text="freeCodeCamp provided me with the skills and confidence I needed to transition into web development. The community support and hands-on projects were invaluable."
+          />
 
-        <Testimony
-          name="Emma"
-          country="Canada"
-          // image="Emma"
-          position="Data Scientist"
-          enterprice="Microsoft"
-          text="The comprehensive curriculum at freeCodeCamp gave me a solid foundation in data science. I'm now working as a data scientist at Microsoft."
-        />
+          <Testimony
+            name="Emma"
+            country="Canada"
+            position="Data Scientist"
+            enterprice="Microsoft"
+            text="The comprehensive curriculum at freeCodeCamp gave me a solid foundation in data science. I'm now working as a data scientist at Microsoft."
+          />
 
+        </div>
       </div>
     </>
   )
--- 01-testimonios/src/components/Testimony.jsx
+++ 01-testimonios/src/components/Testimony.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../stylesheets/Testimony.css'
 import imageShawn from "../assets/images/imageShawn.png"
 import imageSarah from "../assets/images/imageSarah.png"
 import imageEmma from "../assets/images/imageEmma.png"
@@ -15,8 +16,8 @@ const Testimony = (props) => {
       <img 
         className="image-testimony" src={images[props.name]} alt={`image-${props.name}`} />
       <div className="text-container">
-        <p className="name-testimony">{props.name} in {props.country}</p>
-        <p className="position-testimony" > {props.position} at {props.enterprice}</p>
+        <p className="name-testimony"><strong>{props.name}</strong> in {props.country}</p>
+        <p className="position-testimony" > {props.position} at <strong>{props.enterprice}</strong></p>
         <p className="text-testimony">"{props.text}"</p>
       </div>
     </div>
